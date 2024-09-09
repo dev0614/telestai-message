7ce55718100532085e2d248022ad4ed9c27fac32
dev0614 <shimamuratakehiko44@gmail.com>
2024-09-09 05:12:09 -0400

    initial

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+export interface SignatureOptions {
+    segwitType?: 'p2wpkh' | 'p2sh(p2wpkh)';
+    extraEntropy?: Buffer;
+  }
+  
+  export interface Signer {
+    sign(hash: Buffer, extraEntropy?: Buffer): { signature: Buffer; recovery: number };
+  }
+  
+  export interface SignerAsync {
+    sign(hash: Buffer, extraEntropy?: Buffer): Promise<{ signature: Buffer; recovery: number }>;
+  }
+  
+  export function magicHash(message: string | Buffer, messagePrefix?: string): Buffer;
+  
+  export function sign(
+    message: string | Buffer,
+    privateKey: Buffer | Signer,
+    compressed?: boolean,
+    sigOptions?: SignatureOptions
+  ): Buffer;
+  
+  export function signAsync(
+    message: string | Buffer,
+    privateKey: Buffer | SignerAsync | Signer,
+    compressed?: boolean,
+    sigOptions?: SignatureOptions
+  ): Promise<Buffer>;
+  
+  export function verify(
+    message: string | Buffer,
+    address: string,
+    signature: string | Buffer,
+    messagePrefix?: string,
+    checkSegwitAlways?: boolean
+  ): boolean;
+  
